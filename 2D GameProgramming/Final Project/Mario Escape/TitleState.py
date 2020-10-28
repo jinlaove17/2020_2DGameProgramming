056583e73f95ae8c5f1ec829270ac73d63ce6e47
jinlaove17 <jinlaove17@naver.com>
2020-10-28 13:07:10 +0900

    메뉴를 구현하지 않은 title state 구현

---
+++ 2D GameProgramming/Final Project/Mario Escape/TitleState.py
@@ -0,0 +1,37 @@
+from pico2d import *
+import GameFramework
+import GameState
+
+def enter():
+	global background
+
+	background = load_image("Image/TitleState.png")
+
+def update():
+	pass
+
+def draw():
+	background.draw(400, 300)
+
+def handle_event(event):
+	global running
+
+	if (event.type == SDL_QUIT):
+		GameFramework.quit()
+	elif (event.type, event.key) == (SDL_KEYDOWN, SDLK_ESCAPE):
+		GameFramework.quit()
+	elif (event.type, event.key) == (SDL_KEYDOWN, SDLK_SPACE):
+		GameFramework.push(GameState)
+
+def exit():
+	global background
+	del background
+
+def pause():
+	pass
+
+def resume():
+	pass
+
+if (__name__ == "__main__"):
+	GameFramework.run_main()
