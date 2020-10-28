474e3450c9ba3092eba05d3ea86d1027358e8534
jinlaove17 <jinlaove17@naver.com>
2020-10-28 13:01:04 +0900

    image의 rect를 json을 통해 구하고 객체 생성

---
+++ 2D GameProgramming/Final Project/Mario Escape/GameSprite.py
@@ -0,0 +1,33 @@
+import json
+import GameFramework
+from pico2d import *
+
+structure_image = None
+sprite_rects = {}
+
+def load():
+	global structure_image
+
+	if (structure_image is None):
+		structure_image = load_image("Image/Structure.png")
+
+		with open("JSON/ObjectRect.json") as file:
+			data = json.load(file)
+
+			for name in data:
+				print(name)
+				sprite_rects[name] = tuple(data[name])
+
+class Sprite:
+	def __init__(self, name, x, y, w, h):
+		load()
+		self.name = name
+		self.rect = sprite_rects[name]
+		self.pos = (x, y)
+		self.size = (w, h)
+
+	def draw(self):
+		structure_image.clip_draw_to_origin(*self.rect, *self.pos, *self.size)
+
+	def update(self):
+		pass
