43a5d2f5b7f10f20a54b74f543a9e6395c51894a
jinlaove17 <jinlaove17@naver.com>
2020-11-05 01:26:01 +0900

    상호작용 객체 리스트 생성 충돌체크 추가

--- 2D GameProgramming/Final Project/Mario Escape/GameState.py
+++ 2D GameProgramming/Final Project/Mario Escape/GameState.py
@@ -3,9 +3,11 @@ from Mario import *
 import GameFramework
 import GameWorld
 import GameSprite
+import GameObject
 import json
 
 stage_level = None # 스테이트 전환시 level을 변경해가며 enter()를 다시 호출(나중에 사용함)
+interact_object = []
 
 def enter():
 	global mario, tile1, tile2, ladder
@@ -20,11 +22,24 @@ def enter():
 		object = GameSprite.Sprite(info["name"], info["x"], info["y"], info["w"], info["h"])
 		GameWorld.add(info["layer_index"], object)
 
+		if "ladder" in info["name"]:
+			interact_object.append(object)
+
+	print(interact_object)
+
 def update():
 	GameWorld.update()
 
+	for layer_objects in GameWorld.objects:
+		for object in layer_objects:
+			if(object != mario):
+				if GameObject.collides_box(mario, object):
+					#print("Player Collision", object.name)
+					pass
+
 def draw():
 	GameWorld.draw()
+	GameObject.draw_collision_box()
 
 def handle_event(event):
 	global running
