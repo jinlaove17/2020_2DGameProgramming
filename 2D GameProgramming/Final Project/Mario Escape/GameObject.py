d37daf962a89ac96a665eeb6a42dde54540b39f3
jinlaove17 <jinlaove17@naver.com>
2020-11-13 00:58:53 +0900

    충돌체크 함수, 충돌체크 박스 함수를 갖는 Object 모듈 추가

---
+++ 2D GameProgramming/Final Project/Mario Escape/GameObject.py
@@ -0,0 +1,19 @@
+from pico2d import *
+import GameWorld
+
+def collides_box(a, b):
+	(la, ba, ra, ta) = a.get_bb()
+	(lb, bb, rb, tb) = b.get_bb()
+
+	if la > rb: return False
+	if ra < lb: return False
+	if ba > tb: return False
+	if ta < bb: return False
+
+	return True
+
+def draw_collision_box():
+	for layer_objects in GameWorld.objects:
+		for object in layer_objects:
+			if hasattr(object, 'get_bb'):
+				draw_rectangle(*object.get_bb())
