3f0c8c7cfdc68ba3a5ba14911ebc12793db7f495
jinlaove17 <jinlaove17@naver.com>
2020-11-14 01:15:54 +0900

    objects를 curr_object로 나누고 stage 변경 시, 그에맞는 object값을 curr로 초기화

--- 2D GameProgramming/Final Project/Mario Escape/GameWorld.py
+++ 2D GameProgramming/Final Project/Mario Escape/GameWorld.py
@@ -1,39 +1,47 @@
 from pico2d import *
 import GameFramework
 
-objects = []
+curr_objects = []
+stage1_objects = []
+stage2_objects = []
 trashcan = []
 
 def init(layer_names):
-    global objects
+    global curr_objects, stage1_objects, stage2_objects
     global layer
 
-    objects = []
+    curr_objects = []
     layer = lambda: None
     layerIndex = 0
 
     for name in layer_names:
-        objects.append([])
+        curr_objects.append([])
         layer.__dict__[name] = layerIndex
         layerIndex += 1
 
-def add(layer_index, object):
-    objects[layer_index].append(object)
+    stage1_objects = curr_objects
+    stage2_objects = curr_objects
+
+def add(stage_level, layer_index, object):
+    if (stage_level == 1):
+        stage1_objects[layer_index].append(object)
+    elif (stage_level == 2):
+        stage2_objects[layer_index].append(object)
 
 def remove(object):
     trashcan.append(object)
 
 def all_objects():
-    for layer_objects in objects:
+    for layer_objects in curr_objects:
         for object in layer_objects:
             yield object
 
 def object(layer_index, object_index):
-    layer_objects = objects[layer_index]
+    layer_objects = curr_objects[layer_index]
     return layer_objects[object_index]
 
 def objects_at(layer_index):
-    for object in objects[layer_index]:
+    for object in curr_objects[layer_index]:
         yield object
 
 def update():
@@ -54,7 +62,7 @@ def empty_trashcan():
     global trashcan
 
     for object in trashcan:
-        for layer_objects in objects:
+        for layer_objects in curr_objects:
             try:
                 layer_objects.remove(object)
             except ValueError:
