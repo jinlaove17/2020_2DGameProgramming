7cac9577da2c5a923a729b4bb3a43e2a7986517f
jinlaove17 <jinlaove17@naver.com>
2020-11-15 21:34:59 +0900

    __init__에서 file명을 입력받게하고 set_rect로 background 크기 조정

--- 2D GameProgramming/Final Project/Mario Escape/Background.py
+++ 2D GameProgramming/Final Project/Mario Escape/Background.py
@@ -2,8 +2,8 @@ from pico2d import *
 import Image
 
 class Background:
-    def __init__(self):
-        self.image = Image.load("IMAGE/Background.png")
+    def __init__(self, file):
+        self.image = Image.load(file)
         self.cw, self.ch = get_canvas_width(), get_canvas_height()
         self.rect = 0, 0, self.cw, self.ch
 
@@ -13,7 +13,7 @@ class Background:
     def update(self):
         pass
 
-    def set_rect(self, x):
+    def set_rect(self, x, y=0):
         self.cw, self.ch = get_canvas_width(), get_canvas_height()
         cw, ch = self.cw, self.ch
-        self.rect = x, 0, cw, ch
+        self.rect = x, y, cw, ch
