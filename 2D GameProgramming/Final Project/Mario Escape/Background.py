176982dc49bd1ce13cf3f618de31ad55c1649c97
jinlaove17 <jinlaove17@naver.com>
2020-11-20 00:27:53 +0900

    Mario의 위치에따라 좌, 우로 움직이는 배경이 되도록 설정

--- 2D GameProgramming/Final Project/Mario Escape/Background.py
+++ 2D GameProgramming/Final Project/Mario Escape/Background.py
@@ -2,18 +2,26 @@ from pico2d import *
 import Image
 
 class Background:
-    def __init__(self, file):
+    STAGE_LEVEL = 1
+
+    def __init__(self, file, player=None):
         self.image = Image.load(file)
-        self.cw, self.ch = get_canvas_width(), get_canvas_height()
-        self.rect = 0, 0, self.cw, self.ch
+        self.mario = player
 
     def draw(self):
-        self.image.clip_draw_to_origin(*self.rect, 0, 0)
+        # GameState Background
+        if (self.mario != None):
+            rect = (50 * (Background.STAGE_LEVEL - 1), 0, get_canvas_width() + 100 * Background.STAGE_LEVEL, get_canvas_height())
+            (x, y) = (get_canvas_width() // 2, get_canvas_height() // 2)
+            (mx, my) = self.mario.pos
+            (dx, dy) = (x - mx, y - my)
+            self.image.clip_draw(*rect, x + 0.1 * dx, y)
+        # TitleState Background
+        else:                   
+            (w, h) = (get_canvas_width(), get_canvas_height())
+            rect = (250, 80, w, h)
+            pos = (0, 0)
+            self.image.clip_draw_to_origin(*rect, *pos)
 
     def update(self):
         pass
-
-    def set_rect(self, x, y=0):
-        self.cw, self.ch = get_canvas_width(), get_canvas_height()
-        cw, ch = self.cw, self.ch
-        self.rect = x, y, cw, ch
