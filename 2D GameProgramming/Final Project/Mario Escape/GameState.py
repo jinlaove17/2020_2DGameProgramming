d60382c97cdd2576fc80ae0443d462a8f9f6fe26
jinlaove17 <jinlaove17@naver.com>
2020-11-21 22:24:51 +0900

    Stage5(히든 스테이트) 준비 및 랜덤박스와 바위 충돌시 Stage3에 사다리 생기도록 구현

--- 2D GameProgramming/Final Project/Mario Escape/GameState.py
+++ 2D GameProgramming/Final Project/Mario Escape/GameState.py
@@ -32,7 +32,7 @@ def draw():
 
 	GameWorld.draw()
 	GameObject.draw_collision_box()
-	font.draw(40, get_canvas_height() - 57, "X %d" % TOTAL_COIN_COUNT, FONT_COLOR)
+	font.draw(95, get_canvas_height() - 60, "X %d" % TOTAL_COIN_COUNT, FONT_COLOR)
 
 
 def handle_event(event):
@@ -44,7 +44,7 @@ def handle_event(event):
 	mario.handle_event(event)
 
 def exit():
-	global font, bgm, jump_wav, coin_wav, life_lost_wav
+	global font, bgm, start_wav, jump_wav, coin_wav, life_lost_wav
 
 	Image.unload("IMAGE/Mario.png")
 	Image.unload("IMAGE/Sprite.png")
@@ -54,6 +54,7 @@ def exit():
 
 	bgm.stop()
 	del bgm
+	del start_wav
 	del jump_wav
 	del coin_wav
 	del life_lost_wav
@@ -68,14 +69,14 @@ def init_stage():
 	global STAGE_LEVEL, TOTAL_COIN_COUNT
 	global mario, background, ui, font
 
-	STAGE_LEVEL = 1
+	STAGE_LEVEL = 4
 	TOTAL_COIN_COUNT = 0
 	mario = Mario()
 	background = Background("IMAGE/Background.png", mario)
-	ui = GameSprite.UI(10, get_canvas_height() - 34)
+	ui = GameSprite.UI(10, get_canvas_height() - 72)
 	font = Font.load("FONT/koverwatch.ttf", 25)
 
-	for level in range(1, 4 + 1):
+	for level in range(1, 5 + 1):
 		with open("JSON/Stage_%d.json" % level) as file:
 			data = json.load(file)
 
@@ -98,20 +99,24 @@ def init_stage():
 		GameWorld.add(GameWorld.layer.background, background, level)
 		GameWorld.add(GameWorld.layer.ui, ui, level)
 			
-	GameWorld.curr_objects = GameWorld.stage1_objects
+	GameWorld.curr_objects = GameWorld.stage4_objects
 
 def load_sound():
-	global bgm, jump_wav, coin_wav, life_lost_wav, spike_wav
+	global bgm, start_wav, jump_wav, coin_wav, life_lost_wav, spike_wav, plant_attack_wav, plant_dead_wav
 
 	bgm = load_music("SOUND/cave dungeon.mp3")
+	start_wav = load_wav("SOUND/game start.wav")
 	jump_wav = load_wav("SOUND/jump.wav")
 	coin_wav = load_wav("SOUND/coin.wav")
 	life_lost_wav = load_wav("SOUND/life lost.wav")
-	spike_wav = load_wav("SOUND/spike.wav")
+	#spike_wav = load_wav("SOUND/spike.wav")
+	plant_attack_wav = load_wav("SOUND/plant attack.wav")
+	plant_dead_wav = load_wav("SOUND/plant dead.wav")
 
 	bgm.set_volume(100)
 	bgm.repeat_play()
 	life_lost_wav.set_volume(60)
+	start_wav.play()
 
 def check_collision():
 	global TOTAL_COIN_COUNT
@@ -145,11 +150,14 @@ def check_collision():
 				if GameObject.collides_box(obstacle, plant):
 					GameWorld.remove(obstacle)
 					plant.state = GameSprite.Plant.DIE
+					plant_dead_wav.play()
 					return
 			for box in GameWorld.objects_at(GameWorld.layer.box):
 				if GameObject.collides_box(obstacle, box):
 					GameWorld.remove(obstacle)
 					GameWorld.remove(box)
+					ladder = GameSprite.Platform("Ladder_1", 740, 430, 39, 170)
+					GameWorld.add(GameWorld.layer.platform, ladder, 3)
 					return
 
 def set_stage(stage):
@@ -167,6 +175,7 @@ def change_stage():
 
 	(x, y) = mario.pos
 	hw = Mario.IMAGE_RECT[mario.state][mario.fidx % len(Mario.IMAGE_RECT[mario.state])][2] // 2
+	hh = Mario.IMAGE_RECT[mario.state][mario.fidx % len(Mario.IMAGE_RECT[mario.state])][3] // 2
 
 	if (STAGE_LEVEL == 1):
 		if (x < hw):
@@ -193,10 +202,23 @@ def change_stage():
 			x = hw
 			set_stage(NEXT)
 			GameWorld.curr_objects = GameWorld.stage4_objects
+		elif ((730 < x and x < get_canvas_width()) and (y > get_canvas_width())):
+			y = hh
+			mario.delta = (0, 0)
+			STAGE_LEVEL = 5
+			GameWorld.curr_objects = GameWorld.stage5_objects
 	elif (STAGE_LEVEL == 4):
 		if (x < hw):
 			x = get_canvas_width() - hw
 			set_stage(PREV)
+			GameWorld.curr_objects = GameWorld.stage3_objects
+		elif (x > get_canvas_width() - hw):
+			x = get_canvas_width() - hw
+	elif (STAGE_LEVEL == 5):
+		if (y < hh):
+			y = get_canvas_width() - hh
+			mario.delta = (0, 0)
+			STAGE_LEVEL = 3
 			GameWorld.curr_objects = GameWorld.stage3_objects
 			
 	mario.pos = (x, y)
