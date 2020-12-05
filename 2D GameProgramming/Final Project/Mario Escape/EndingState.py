fce2a6ce9d3a68457c0ecb0dea2711f6531807cf
jinlaove17 <jinlaove17@naver.com>
2020-12-05 12:03:40 +0900

    폰트 크기 변경 및 글자 자리 배치 수정

--- 2D GameProgramming/Final Project/Mario Escape/EndingState.py
+++ 2D GameProgramming/Final Project/Mario Escape/EndingState.py
@@ -12,7 +12,7 @@ def enter():
 
 	global font
 	
-	font = Font.load("FONT/koverwatch.ttf", 30)
+	font = Font.load("FONT/koverwatch.ttf", 35)
 
 	global bgm, clear_wav
 
@@ -36,11 +36,12 @@ def draw():
 
 	FPS = 30
 	y += FPS * GameFramework.delta_time
-	font.draw(480, y, "Mario escaped successfully !", FONT_COLOR)
-	font.draw(540, y - 100, "< MARIO ESCAPE >", FONT_COLOR)
-	font.draw(480, y - 160, "DIRECTOR : 2017180038 전종우", FONT_COLOR)
-	font.draw(450, y - 220, "2D Game Programming Final Project", FONT_COLOR)
-	font.draw(560, y - 400, "THANK YOU !", FONT_COLOR)
+	font.draw(400, y, "Mario escaped successfully !", FONT_COLOR)
+	font.draw(470, y - 100, "< MARIO ESCAPE >", FONT_COLOR)
+	font.draw(400, y - 160, "DIRECTOR : 2017180038 전종우", FONT_COLOR)
+	font.draw(410, y - 220, "최종 수정일 : 2020-12-05(토)", FONT_COLOR)
+	font.draw(360, y - 280, "2D Game Programming Final Project", FONT_COLOR)
+	font.draw(505, y - 480, "THANK YOU !", FONT_COLOR)
 
 def handle_event(event):
 	if (event.type == SDL_QUIT):
